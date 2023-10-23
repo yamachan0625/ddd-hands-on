3d757287b73bc98e691fffbf9110ca8c8001184e
yamachan0625 <napoli.080625@gmail.com>
2023-10-23 23:42:26 +0900

    環境のセットアップ

---
+++ StockManagementDomain/jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleDirectories: ['node_modules', 'src'],
+  transformIgnorePatterns: ['/node_modules'],
+};
