a19605302295e82acc10a0e57c45996602fa80ab
yamachan0625 <napoli.080625@gmail.com>
2023-11-07 23:35:12 +0900

    ビジネスルール修正

--- src/Domain/models/Book/Stock/Stock.test.ts
+++ src/Domain/models/Book/Stock/Stock.test.ts
@@ -1,5 +1,4 @@
 import { Stock } from './Stock';
-import { BookId } from '../BookId/BookId';
 import { QuantityAvailable } from './QuantityAvailable/QuantityAvailable';
 import { Status, StatusEnum } from './Status/Status';
 import { StockId } from './StockId/StockId';
@@ -10,19 +9,17 @@ jest.mock('nanoid', () => ({
 }));
 
 describe('Stock', () => {
-  const bookId = new BookId('9784167158057');
   const stockId = new StockId('abc');
   const quantityAvailable = new QuantityAvailable(10);
   const status = new Status(StatusEnum.OnSale);
 
   describe('create', () => {
     it('デフォルト値で在庫を作成する', () => {
-      const stock = Stock.create(bookId);
+      const stock = Stock.create();
 
       expect(
         stock.stockId.equals(new StockId('testIdWithExactLength'))
       ).toBeTruthy();
-      expect(stock.bookId.equals(bookId)).toBeTruthy();
       expect(
         stock.quantityAvailable.equals(new QuantityAvailable(0))
       ).toBeTruthy();
@@ -32,12 +29,7 @@ describe('Stock', () => {
 
   describe('delete', () => {
     it('在庫が販売中の場合はエラーを投げる', () => {
-      const stock = Stock.reconstruct(
-        stockId,
-        bookId,
-        quantityAvailable,
-        status
-      );
+      const stock = Stock.reconstruct(stockId, quantityAvailable, status);
 
       expect(() => stock.delete()).toThrow('販売中の在庫は削除できません。');
     });
@@ -46,7 +38,6 @@ describe('Stock', () => {
       const notOnSaleStatus = new Status(StatusEnum.Discontinued);
       const stock = Stock.reconstruct(
         stockId,
-        bookId,
         quantityAvailable,
         notOnSaleStatus
       );
@@ -57,12 +48,7 @@ describe('Stock', () => {
 
   describe('increaseQuantity', () => {
     it('在庫数を増やす', () => {
-      const stock = Stock.reconstruct(
-        stockId,
-        bookId,
-        quantityAvailable,
-        status
-      );
+      const stock = Stock.reconstruct(stockId, quantityAvailable, status);
       stock.increaseQuantity(5);
 
       expect(
@@ -71,12 +57,7 @@ describe('Stock', () => {
     });
 
     it('増加量が負の数の場合はエラーを投げる', () => {
-      const stock = Stock.reconstruct(
-        stockId,
-        bookId,
-        quantityAvailable,
-        status
-      );
+      const stock = Stock.reconstruct(stockId, quantityAvailable, status);
 
       expect(() => stock.increaseQuantity(-1)).toThrow(
         '増加量は0以上でなければなりません。'
@@ -86,12 +67,7 @@ describe('Stock', () => {
 
   describe('decreaseQuantity', () => {
     it('在庫数を減らす', () => {
-      const stock = Stock.reconstruct(
-        stockId,
-        bookId,
-        quantityAvailable,
-        status
-      );
+      const stock = Stock.reconstruct(stockId, quantityAvailable, status);
       stock.decreaseQuantity(5);
 
       expect(
@@ -100,12 +76,7 @@ describe('Stock', () => {
     });
 
     it('減少量が負の数の場合はエラーを投げる', () => {
-      const stock = Stock.reconstruct(
-        stockId,
-        bookId,
-        quantityAvailable,
-        status
-      );
+      const stock = Stock.reconstruct(stockId, quantityAvailable, status);
 
       expect(() => stock.decreaseQuantity(-1)).toThrow(
         '減少量は0以上でなければなりません。'
@@ -113,23 +84,13 @@ describe('Stock', () => {
     });
 
     it('減少後の在庫数が0未満になる場合はエラーを投げる', () => {
-      const stock = Stock.reconstruct(
-        stockId,
-        bookId,
-        quantityAvailable,
-        status
-      );
+      const stock = Stock.reconstruct(stockId, quantityAvailable, status);
 
       expect(() => stock.decreaseQuantity(11)).toThrow();
     });
 
     it('在庫数が0になったらステータスを販売停止にする', () => {
-      const stock = Stock.reconstruct(
-        stockId,
-        bookId,
-        quantityAvailable,
-        status
-      );
+      const stock = Stock.reconstruct(stockId, quantityAvailable, status);
       stock.decreaseQuantity(10);
 
       expect(
--- src/Domain/models/Book/Stock/Stock.ts
+++ src/Domain/models/Book/Stock/Stock.ts
@@ -1,4 +1,3 @@
-import { BookId } from '../BookId/BookId';
 import { QuantityAvailable } from './QuantityAvailable/QuantityAvailable';
 import { Status, StatusEnum } from './Status/Status';
 import { StockId } from './StockId/StockId';
@@ -6,23 +5,17 @@ import { StockId } from './StockId/StockId';
 export class Stock {
   private constructor(
     private readonly _stockId: StockId,
-    private readonly _bookId: BookId,
     private _quantityAvailable: QuantityAvailable,
     private _status: Status
   ) {}
 
   // 新規エンティティの生成
-  static create(bookId: BookId) {
+  static create() {
     const defaultStockId = new StockId(); // 自動ID採番
     const defaultQuantityAvailable = new QuantityAvailable(0);
     const defaultStatus = new Status(StatusEnum.PreSale);
 
-    return new Stock(
-      defaultStockId,
-      bookId,
-      defaultQuantityAvailable,
-      defaultStatus
-    );
+    return new Stock(defaultStockId, defaultQuantityAvailable, defaultStatus);
   }
 
   delete() {
@@ -67,19 +60,14 @@ export class Stock {
   // エンティティの再構築
   static reconstruct(
     stockId: StockId,
-    bookId: BookId,
     quantityAvailable: QuantityAvailable,
     status: Status
   ) {
-    return new Stock(stockId, bookId, quantityAvailable, status);
+    return new Stock(stockId, quantityAvailable, status);
   }
 
   get stockId(): StockId {
     return this._stockId;
-  }
-
-  get bookId(): BookId {
-    return this._bookId;
   }
 
   get quantityAvailable(): QuantityAvailable {
